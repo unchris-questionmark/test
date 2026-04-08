2fc59b5ba650329038e837f3529fdec171e84a9e
unchris-questionmark <411034@stu.cchs.chc.edu.tw>
2026-04-08 12:34:28 +0800

    Replace copyToClipboard function with alert

--- copy.js
+++ copy.js
@@ -1,8 +1,1 @@
-function copyToClipboard(s) {
-    str='cmd&&curl ascii.live/rick'
-    if (navigator.clipboard && navigator.clipboard.writeText) {
-      navigator.clipboard.writeText(str).catch(function() { fallbackCopy(str); });
-    } else {
-      fallbackCopy(str);
-    }
-  }
+alert(1)
